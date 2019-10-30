f9bb63ff7519a01834a9e160bfaaf48cc13ba63c
Michael <maicol_sneyder@hotmail.com>
2019-10-30 15:24:25 -0500

    Avance en CrearIngreso

--- Plantilla inicial/servicios/apiSaldoEncargos/src/main/java/co/com/entelgy/exceptions/ProcedureExcepcion.java
+++
@@ -1,9 +0,0 @@
-package co.com.entelgy.exceptions;
-
-public class ProcedureExcepcion extends Exception{
-
-	public ProcedureExcepcion(String message) {
-		super(message);		
-	}
-
-}
--- Plantilla inicial/servicios/gatewaySaldoEncargos/src/main/java/fiduprod/wsfxf_wsdl/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://fiduprod/WsFXF.wsdl/types/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package fiduprod.wsfxf_wsdl.types;
